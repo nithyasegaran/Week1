ffdeebe6b538921132c836f4e513f1960dbbdb2b
nithyasegaran <nithyasegaran@yahoo.com>
2026-05-25 15:31:04 +0800

    Add files via upload

---
+++ OddEven.js
@@ -0,0 +1,18 @@
+// to print odd numbers from 1 to 20
+
+for (let i = 1; i <= 20; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
+
+// to print even numbers from 1 to 20
+
+for (let i = 1; i <= 20; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+} 
+
+
+
